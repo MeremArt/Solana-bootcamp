1ad3345f1f4f56d9e2fd4dedddbf4f3207d5916b
Ugo Chinemerem <ugofranklin22@gmail.com>
2024-08-18 00:15:45 +0100

    add:hashing_maps

--- homeworks_rust/homeworks/homework6/collections/hashmap2.rs
+++ homeworks_rust/homeworks/homework6/collections/hashmap2.rs
@@ -38,6 +38,12 @@ fn fruit_basket(basket: &mut HashMap<Fruit, u32>) {
         // TODO: Put new fruits if not already present. Note that you
         // are not allowed to put any type of fruit that's already
         // present!
+        match fruit {
+            Fruit::Banana => basket.entry(fruit).or_insert(3),
+            Fruit::Pineapple => basket.entry(fruit).or_insert(4),
+            // Skip if it's an existing fruit
+            _ => &mut 1,
+        };
     }
 }
 
